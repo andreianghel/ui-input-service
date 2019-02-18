a209bf412aa23068a151b28d593d80c0666bbe75
Andrei Anghel <Andrei.Anghel@belden.com>
2019-02-18 18:19:25 +0200

    added unimplemented methods

--- src/main/java/ro/andreianghel/uiinputservice/controllers/UIController.java
+++ src/main/java/ro/andreianghel/uiinputservice/controllers/UIController.java
@@ -1,15 +1,24 @@
 package ro.andreianghel.uiinputservice.controllers;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UIController {
 
-    @PostMapping("\newEntry")
+    @PostMapping("entry")
     public static void addNewEntry(@RequestBody String entryString) {
-        //FIXME put the string on a message queue
+        // FIXME put the string on a message queue as a create message
+    }
+
+    @PutMapping("entry/{id}")
+    public void updateEntry(@PathVariable("id") Integer id, @RequestBody String entryString) {
+        // FIXME put a message with the id and the new content of the entry
+    }
+
+    @DeleteMapping("entry/{id}")
+    public void deleteEntry(@PathVariable("id") Integer id) {
+        // FIXME implement the delete request
+        // put a request message on the queue
     }
 
 }
