90d62a9f2198d5b0f3f7b42e2041f255e88acdca
Andrei Anghel <Andrei.Anghel@belden.com>
2019-02-20 00:11:58 +0200

    added preliminary support for kafka messaging
    Signed-off-by: andrei.anghel <ndr.anghel@gmail.com>

---
+++ src/main/java/ro/andreianghel/uiinputservice/MessageProducerService.java
@@ -0,0 +1,33 @@
+package ro.andreianghel.uiinputservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageProducerService {
+
+    public static final String TOPIC_ADD = "add_entry";
+    public static final String TOPIC_GET_ALL = "get_all_entries";
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public MessageProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendAddMessage(String message) {
+        sendMessage(TOPIC_ADD, message);
+    }
+
+    public void sendGetAllMessage() {
+        sendMessage(TOPIC_GET_ALL, "");
+    }
+
+    public void sendMessage(String topic, String message) {
+       // System.out.println(String.format("#### -> Producing message -> %s", message));
+        this.kafkaTemplate.send(topic, message);
+    }
+
+}
--- src/main/java/ro/andreianghel/uiinputservice/controllers/UIController.java
+++ src/main/java/ro/andreianghel/uiinputservice/controllers/UIController.java
@@ -1,9 +1,36 @@
 package ro.andreianghel.uiinputservice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ro.andreianghel.uiinputservice.MessageProducerService;
 
 @RestController
 public class UIController {
+
+    private MessageProducerService messageProducerService;
+
+    private int counter;
+
+    @Autowired
+    public UIController(MessageProducerService messageProducerService) {
+        this.messageProducerService = messageProducerService;
+        this.counter = 0;
+    }
+
+
+    @GetMapping("demoAdd")
+    String demoAdd() {
+
+        // FIXME push a message to a kafka consumer
+
+        messageProducerService.sendAddMessage("demo message: "+(counter++));
+
+
+
+
+        return "hi";
+    }
+
 
     @PostMapping("entry")
     public static void addNewEntry(@RequestBody String entryString) {
